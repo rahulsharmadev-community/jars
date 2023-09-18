f64f274f1dfbd7e6a43eb9623c4dd277e3337b59
RAHUL <rahulsharmadev.github@gmail.com>
2023-09-19 05:26:07 +0530

    add guard, improve Extension lib

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -5,7 +5,7 @@ export 'src/widgets/gap/gap.dart';
 export 'src/widgets/jdialogs/jdialogs.dart';
 export 'src/widgets/jsnackbar_jtoast/jsnackbar_jtoast.dart';
 
-export 'src/widgets/marquee/marquee.dart';
+export 'src/widgets/jmarquee/jmarquee.dart';
 export 'src/widgets/floating_textfield.dart';
 export 'src/widgets/slide_menu_button.dart';
 
